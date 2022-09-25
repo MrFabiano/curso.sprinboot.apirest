65e34ce2b7c626a38fc980bde189c2f7cbfc2644
fabiano <fabianosanttoss@hotmail.com>
2022-09-25 01:28:51 -0300

    refatorando codigo

--- src/main/java/com/springboot/curso/sprinboot/apirest/model/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.curso.sprinboot.apirest.model;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface ClientRepository extends MongoRepository<Client, String> {
-}

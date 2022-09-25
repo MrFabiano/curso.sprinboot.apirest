9fcf337b7c76567b5cd400057971c8325bde4442
fabiano <fabianosanttoss@hotmail.com>
2022-09-25 01:39:39 -0300

    incluindo cliente

---
+++ src/main/java/com/springboot/curso/sprinboot/apirest/model/Client.java
@@ -0,0 +1,14 @@
+package com.springboot.curso.sprinboot.apirest.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Client {
+
+    private Long id;
+    private String nome;
+    private int idade;
+    
+}
